3f892d00d0cf215785109752ceb0c47d62d2ecd0
Matteo Lupica <matteo.lupica@edu.hefr.ch>
2025-02-21 14:30:46 +0100

    Update config settings with new file paths and default parameters
    - Introduce FILE_XLS for player stats Excel file location.
    - Add DEFAULT_NUMBER_OF_GAMES for match retrieval limit.
    - Add USERS_FILE for managing user data storage.

--- config/config.py
+++ config/config.py
@@ -2,8 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 REGION = "europe"
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+FILE_XLS = "../data/player_stats.xlsx"
+DEFAULT_NUMBER_OF_GAMES = 20
+USERS_FILE = "../data/users.json"
